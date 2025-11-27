64b02894af85db01688f1a43de182261c3894a44
Jaksa Márkó <jaksa.marko@diak.szbi-pg.hu>
2025-11-27 13:53:24 +0100

    Az

--- konyvtar_adatbazis.sql
+++ konyvtar_adatbazis.sql
@@ -1,4 +1,5 @@
 -- 2. feladat 
+DROP DATABASE IF EXISTS konyvtaradatbazis;
 CREATE DATABASE konyvtaradatbazis
     CHARACTER SET utf8mb4 
     COLLATE utf8mb4_hungarian_ci;
@@ -6,21 +7,27 @@ CREATE DATABASE konyvtaradatbazis
     -- 3. feladat 
 USE konyvtaradatbazis;
     -- 4. feladat 
-CREATE TABLE konyvek (
+DROP TABLE IF EXISTS konyvek;
+CREATE TABLE IF NOT EXISTS konyvek (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     cim VARCHAR(200) NOT NULL UNIQUE,
     szerzo VARCHAR(120),
     kiadasi_ev YEAR, 
-    oldalszam INTEGER CHECK (BETWEEN 20 AND 2000),
-    kategoria ENUM("fantasy", "sci-fi", "disztópia", "történelem")
+    oldalszam INTEGER CHECK (oldalszam BETWEEN 20 AND 2000),
+    kategoria ENUM("fantasy", "sci-fi", "disztópia",
+    "történelem"),
     ertekeles FLOAT DEFAULT 5.0 
 );
     -- 5. feladat
-CREATE TABLE olvasok (
+DROP TABLE IF EXISTS olvasok;
+CREATE TABLE IF NOT EXISTS olvasok (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     nev VARCHAR(100) NOT NULL UNIQUE,
     szuletesi_datum DATE,
-    varos VARCHAR(50) DEFAULT(Budapest),
-    aktiv DEFAULT(igen),
-    regisztracios_idopontja 
+    varos VARCHAR(50) DEFAULT "Budapest",
+    aktiv ENUM ('igen', 'nem') DEFAULT("igen"),
+    regisztracios_idopontja DATETIME DEFAULT CURRENT_TIMESTAMP
 );
+
+    -- 6. feladat
+DESCRIBE konyvek
